a00e40a475f377e0fd754110e3d4376c278bd975
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-03-12 01:43:52 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+let a
+let b
+let n
+function add(){
+ a=1000
+b=804
+return(a + b)
+}
+
+function subtract(){
+    
+    
+return(a - b)
+}
+
+function multiply(){
+
+    return(a*b)
+}
+
+function divide(){
+
+    return(a/b)
+}
+function increment(n){
+
+    return(++n)
+}
+
+function decrement(n){
+
+    return(--n)
+}
+
+function makeInt(n){
+    return(parseInt(n,10))
+}
+
+function preserveDecimal(n){
+    return(parseFloat(n))
+}
